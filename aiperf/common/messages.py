7ae3d5ed680bd8cb1e81754667106fe30dc0804a
Anthony Casagrande <acasagrande@nvidia.com>
2025-06-06 17:55:16 -0700

    add decorator to specify which fields to exclude if none

--- aiperf/common/messages.py
+++ aiperf/common/messages.py
@@ -3,9 +3,9 @@
 
 import time
 import uuid
-from typing import Annotated, Any, Literal
+from typing import Annotated, Any, ClassVar, Literal
 
-from pydantic import BaseModel, Field, TypeAdapter
+from pydantic import BaseModel, Field, TypeAdapter, model_serializer
 
 from aiperf.common.enums import CommandType, MessageType, ServiceState, ServiceType
 
@@ -13,29 +13,67 @@ from aiperf.common.enums import CommandType, MessageType, ServiceState, ServiceT
 # Abstract Base Message Models
 ################################################################################
 
+EXCLUDE_IF_NONE = "__exclude_if_none__"
 
+
+def exclude_if_none(field_names: list[str]):
+    """Decorator to set the _exclude_if_none_fields class attribute to the set of
+    field names that should be excluded if they are None.
+    """
+
+    def decorator(model: type["BaseMessage"]) -> type["BaseMessage"]:
+        model._exclude_if_none_fields.update(field_names)
+        return model
+
+    return decorator
+
+
+@exclude_if_none(["request_ns", "request_id"])
 class BaseMessage(BaseModel):
     """Base message model with common fields for all messages.
+
     Each message model should inherit from this class, set the message_type field,
     and define its own additional fields.
+
+    Optionally, the @exclude_if_none decorator can be used to specify which fields
+    should be excluded from the serialized message if they are None.
+
+    Example:
+    ```python
+    @exclude_if_none(["some_field"])
+    class ExampleMessage(BaseMessage):
+        some_field: int | None = Field(default=None)
+        other_field: int = Field(default=1)
+    ```
+    """
+
+    _exclude_if_none_fields: ClassVar[set[str]] = set()
+    """Set of field names that should be excluded from the serialized message if they
+    are None. This is set by the @exclude_if_none decorator.
     """
 
     request_ns: int | None = Field(
         default=None,
         description="Timestamp of the request",
     )
+
     request_id: str | None = Field(
         default=None,
         description="ID of the request",
     )
 
-    def model_dump_json(self, **kwargs) -> str:
-        """Serialize the message to JSON.
+    @model_serializer
+    def _serialize_message(self) -> dict[str, Any]:
+        """Serialize the message to a dictionary.
 
-        This method overrides the default model_dump_json method to exclude
-        unset fields.
+        This method overrides the default serializer to exclude fields that have a
+        value of None and have the EXCLUDE_IF_NONE json_schema_extra key set to True.
         """
-        return super().model_dump_json(exclude_none=True, **kwargs)
+        return {
+            k: v
+            for k, v in self
+            if not (k in self._exclude_if_none_fields and v is None)
+        }
 
 
 class BaseServiceMessage(BaseMessage):
