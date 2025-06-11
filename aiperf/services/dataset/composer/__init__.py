255182f56f1c02fa1665182deb13ac9d25b75690
Hyunjae Woo <hwoo@nvidia.com>
2025-06-11 11:35:54 -0700

    rename file_input_retriever -> custom

--- aiperf/services/dataset/composer/__init__.py
+++ aiperf/services/dataset/composer/__init__.py
@@ -2,17 +2,13 @@
 #  SPDX-License-Identifier: Apache-2.0
 
 from aiperf.services.dataset.composer.base import BaseConversationComposer
-from aiperf.services.dataset.composer.file_input_retriever import (
+from aiperf.services.dataset.composer.custom import (
     CustomConversationComposer,
-)
-from aiperf.services.dataset.composer.payload_input_retriever import (
-    PayloadInputRetriever,
 )
 from aiperf.services.dataset.composer.synthetic import SyntheticConversationComposer
 
 __all__ = [
     "BaseConversationComposer",
     "CustomConversationComposer",
-    "PayloadInputRetriever",
     "SyntheticConversationComposer",
 ]
