d3bb9165b2336072edccf345f262ae976fc2a9ec
lkomali <lkomali@nvidia.com>
2025-07-07 12:56:41 -0700

    Fix comments

--- aiperf/services/records_manager/metrics/types/request_throughput_metric.py
+++ aiperf/services/records_manager/metrics/types/request_throughput_metric.py
@@ -1,5 +1,6 @@
 #  SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #  SPDX-License-Identifier: Apache-2.0
+from aiperf.common.constants import NANOS_PER_SECOND
 from aiperf.common.enums import MetricTimeType, MetricType
 from aiperf.common.record_models import ParsedResponseRecord
 from aiperf.services.records_manager.metrics.base_metric import BaseMetric
@@ -24,14 +25,15 @@ class RequestThroughputMetric(BaseMetric):
     def update_value(
         self,
         record: ParsedResponseRecord | None = None,
-        metrics: dict["BaseMetric"] | None = None,
+        metrics: dict[str, "BaseMetric"] | None = None,
     ) -> None:
         if record:
+            self._check_record(record)
             self.total_requests += 1
 
         else:
             benchmark_duration = metrics["benchmark_duration"].values()
-            self.metric = self.total_requests / (benchmark_duration / 1e9)
+            self.metric = self.total_requests / (benchmark_duration / NANOS_PER_SECOND)
 
     def values(self) -> float:
         """
@@ -40,4 +42,13 @@ class RequestThroughputMetric(BaseMetric):
         return self.metric
 
     def _check_record(self, record: ParsedResponseRecord) -> None:
-        pass
+        """
+        Checks if the record is valid.
+
+        Raises:
+            ValueError: If the record is None or is invalid.
+        """
+        if not record:
+            raise ValueError("Record must have a valid request.")
+        if not record.valid:
+            raise ValueError("Invalid Record.")
