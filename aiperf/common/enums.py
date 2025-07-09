297dcf7396842eed38e1399b31666b97ffc649db
Anthony Casagrande <acasagrande@nvidia.com>
2025-07-09 14:06:28 -0700

    move creditphase back to enums

--- aiperf/common/enums.py
+++ aiperf/common/enums.py
@@ -660,3 +660,24 @@ class RequestRateMode(CaseInsensitiveStrEnum):
 
     # DYNAMIC = "dynamic"
     # """Generate requests at a dynamic rate based on the average response times of the previous requests. TBD."""
+
+
+class CreditPhase(CaseInsensitiveStrEnum):
+    """The type of credit phase. This is used to identify which phase of the
+    benchmark the credit is being used in, for tracking and reporting purposes."""
+
+    WARMUP = "warmup"
+    """The credit phase is the warmup phase. This is used to warm up the model
+    before the benchmark starts."""
+
+    RAMP_UP = "ramp_up"
+    """The credit phase is the ramp up phase. This is used to ramp up the request
+    rate before the benchmark starts."""
+
+    STABILIZING = "stabilizing"
+    """Used with stabilization based measurement modes to indicate that the
+    benchmark is still stabilizing."""
+
+    STEADY_STATE = "steady_state"
+    """The credit phase is the steady state phase. This is the primary phase of the
+    benchmark, and what is used to calculate the final results."""
