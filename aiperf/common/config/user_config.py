05499dce68df31498eaa8b3d5fbc456f21a63a80
Anthony Casagrande <acasagrande@nvidia.com>
2025-08-12 10:30:26 -0700

    feat: implement genai-perf style artifact-dir naming

--- aiperf/common/config/user_config.py
+++ aiperf/common/config/user_config.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from pathlib import Path
 from typing import Annotated
 
 from pydantic import Field, model_validator
@@ -12,7 +13,8 @@ from aiperf.common.config.input_config import InputConfig
 from aiperf.common.config.loadgen_config import LoadGeneratorConfig
 from aiperf.common.config.output_config import OutputConfig
 from aiperf.common.config.tokenizer_config import TokenizerConfig
-from aiperf.common.enums import RequestRateMode, TimingMode
+from aiperf.common.enums.endpoints_enums import EndpointServiceKind
+from aiperf.common.enums.timing_enums import RequestRateMode, TimingMode
 
 
 class UserConfig(BaseConfig):
@@ -79,7 +81,63 @@ class UserConfig(BaseConfig):
         ),
     ] = LoadGeneratorConfig()
 
-    @property
-    def timing_mode(self) -> TimingMode:
-        """Get the timing mode based on the user config."""
-        return self._timing_mode
+    @model_validator(mode="after")
+    def _compute_config(self) -> Self:
+        """Compute additional configuration.
+
+        This method is automatically called after the model is validated to compute additional configuration.
+        """
+
+        if "artifact_directory" not in self.output.model_fields_set:
+            self.output.artifact_directory = self._compute_artifact_directory()
+
+        return self
+
+    def _compute_artifact_directory(self) -> Path:
+        """Compute the artifact directory based on the user selected options."""
+        names: list[str] = [
+            self._get_artifact_model_name(),
+            self._get_artifact_service_kind(),
+            self._get_artifact_stimulus(),
+        ]
+        return self.output.artifact_directory / Path("-".join(names))
+
+    def _get_artifact_model_name(self) -> str:
+        """Get the artifact model name based on the user selected options."""
+        model_name: str = self.endpoint.model_names[0]
+        if len(self.endpoint.model_names) > 1:
+            model_name = f"{model_name}_multi"
+
+        # Preprocess Huggingface model names that include '/' in their model name.
+        if "/" in model_name:
+            filtered_name = "_".join(model_name.split("/"))
+            from aiperf.common.logging import AIPerfLogger
+
+            _logger = AIPerfLogger(__name__)
+            _logger.info(
+                f"Model name '{model_name}' cannot be used to create artifact "
+                f"directory. Instead, '{filtered_name}' will be used."
+            )
+            model_name = filtered_name
+        return model_name
+
+    def _get_artifact_service_kind(self) -> str:
+        """Get the service kind name based on the endpoint config."""
+        if self.endpoint.type.service_kind == EndpointServiceKind.OPENAI:
+            return f"{self.endpoint.type.service_kind}-{self.endpoint.type}"
+        else:
+            raise ValueError(
+                f"Unknown service kind '{self.endpoint.type.service_kind}'."
+            )
+
+    def _get_artifact_stimulus(self) -> str:
+        """Get the stimulus name based on the timing mode."""
+        match self._timing_mode:
+            case TimingMode.CONCURRENCY:
+                return f"concurrency{self.loadgen.concurrency}"
+            case TimingMode.REQUEST_RATE:
+                return f"request_rate{self.loadgen.request_rate}"
+            case TimingMode.FIXED_SCHEDULE:
+                return "fixed_schedule"
+            case _:
+                raise ValueError(f"Unknown timing mode '{self.timing_mode}'.")
