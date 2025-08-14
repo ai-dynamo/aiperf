1a062b9ff1ea22cd448015b4260bcffe004ff9bc
Hyunjae Woo <hwoo@nvidia.com>
2025-08-14 15:17:50 -0700

    clean up

--- aiperf/dataset/loader/multi_turn.py
+++ aiperf/dataset/loader/multi_turn.py
@@ -116,35 +116,4 @@ class MultiTurnDatasetLoader(AIPerfLoggerMixin, MediaConversionMixin):
                 session_id = multi_turn_data.session_id or str(uuid.uuid4())
                 dataset[session_id].append(multi_turn_data)
 
-        return self._post_process_dataset(dataset)
-
-    def _post_process_dataset(
-        self, dataset: dict[str, list[MultiTurn]]
-    ) -> dict[str, list[MultiTurn]]:
-        """Post-process the dataset to ensure it is valid.
-
-        Returns:
-            The post-processed dataset.
-        """
-        # TODO: implement this
-
-        # merge and change to list
-        # merged_dataset = []
-        # for session_id, multi_turns in dataset.items():
-        #    for multi_turn in multi_turns:
-        #        for turn in multi_turn.turns:
-        #            if turn.texts:
-        #                turn.texts = [Text(name=text.name, contents=text.contents) for text in turn.texts]
-        #            if turn.images:
-        #                turn.images = [Image(name=image.name, contents=image.contents) for image in turn.images]
-        #            if turn.audios:
-        #                turn.audios = [Audio(name=audio.name, contents=audio.contents) for audio in turn.audios]
-        #            if turn.delay:
-        #                turn.delay = Delay(delay=turn.delay)
-        #            if turn.timestamp:
-        #                turn.timestamp = Timestamp(timestamp=turn.timestamp)
-        #            if turn.session_id:
-        #                turn.session_id = session_id
-        #    dataset[session_id] = [MultiTurn(session_id=session_id, turns=turns) for turns in multi_turns]
-
         return dataset
