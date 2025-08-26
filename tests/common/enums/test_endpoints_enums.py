b6fe6c98409430e3d02e48ff45a3b8be570e0504
David Yastremsky <dyastremsky@nvidia.com>
2025-08-26 15:52:49 -0700

    Fix test

--- tests/common/enums/test_endpoints_enums.py
+++ tests/common/enums/test_endpoints_enums.py
@@ -14,7 +14,7 @@ class TestEndpointInfo:
         "tag,supports_streaming,produces_tokens,endpoint_path,metrics_title",
         [
             ("chat", True, True, "/v1/chat/completions", "LLM Metrics"),
-            ("embeddings", True, True, "/v1/embeddings", "Embeddings Metrics"),
+            ("embeddings", False, False, "/v1/embeddings", "Embeddings Metrics"),
             ("custom", True, False, None, None),
             ("test", False, True, "/test", "Test Metrics"),
         ],
